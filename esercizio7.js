08703be6dede644cfb937a5a6efe112a6599c7a9
GiuliaZampa <zampa.giulia@spes.uniud.it>
2018-05-03 15:36:53 +0200

    Esercizio 7 risolto

---
+++ esercizio7.js
@@ -0,0 +1,17 @@
+
+function FunctionMouseOver() {
+    window.alert("Errore Mouse Over")
+}
+
+
+function FunctionMouseOut() {
+    window.alert("Errore Mouse Out")
+}
+
+function FunctionClick() {
+    window.alert("Errore Click")
+}
+
+function FunctionContextMenu() {
+    window.alert("Errore Context Menu")
+}
